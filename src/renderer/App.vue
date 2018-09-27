12aa54ecc5b335c03eb78e3910e2f4d65e44a8af
saikksub <saikksub@gmail.com>
2018-09-27 08:05:20 +0530

    Initial commit with template

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'briefnote'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
